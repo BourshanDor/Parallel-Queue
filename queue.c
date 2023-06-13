9e7890c3c442ea91762ff9bb29267efc7e92e2b9
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-06-13 17:43:36 +0300

    problem in FIFO test

--- queue.c
+++ queue.c
@@ -10,8 +10,10 @@
 
 typedef struct Node
 {
-    void *data;        // Pointer to the data stored in the node
-    struct Node *next; // Pointer to the next node in the linked list
+    void *data;         // Pointer to the data stored in the node
+    cnd_t *thread_hold; // Pointer to the thread_hold stored in the node
+    struct Node *next;  // Pointer to the next node in the linked list
+
 } Node;
 
 typedef struct
@@ -20,6 +22,7 @@ typedef struct
     Node *last;         // Pointer of the last element that arrive
     atomic_size_t size; // Current size of the queue
     bool empty;
+
 } Queue;
 
 Queue jobs;
@@ -29,6 +32,17 @@ atomic_size_t count_visited = 0;
 
 mtx_t lock;
 
+void enterJobToJobsQueue(void *t);
+int wakeUpWorker(void);
+void *waitForAJob(void);
+void *getAJob(void);
+void destroyWorkers(void);
+void destroyJobs(void);
+void initWorkers(void);
+void initJobs(void);
+void removeJob(void *node);
+void removeWorker(void *node);
+void enterWorker(void *node);
 /*
 Need to check if I need to initialized the lock outside the function.
 The reason for that it is that the user can creat a new thread and call to
@@ -46,51 +60,22 @@ void initQueue(void)
     }
     mtx_lock(&lock);
 
-    // Init jobs queue
-    jobs.first = NULL;
-    jobs.last = NULL;
-    jobs.size = 0;
-    jobs.empty = true;
-
-    // Init workers queue
-    workers.first = NULL;
-    workers.last = NULL;
-    workers.size = 0;
-    workers.empty = true;
+    // Init jobs and Workers queue
+    initJobs();
+    initWorkers();
 
     mtx_unlock(&lock);
 }
 
 void destroyQueue(void)
 {
-    Node *tmp;
+
     mtx_lock(&lock);
 
-    // Free jobs queue
-    while (!jobs.empty)
-    {
-        tmp = jobs.first->next;
-        free(jobs.first);
-        jobs.first = tmp;
-        jobs.size--;
-        if (jobs.size == 0)
-        {
-            jobs.empty = true;
-        }
-    }
+    // Free jobs and workers queue
+    destroyJobs();
+    destroyWorkers();
 
-    // Free workers queue
-    while (!workers.empty)
-    {
-        tmp = workers.first->next;
-        free(workers.first);
-        workers.first = tmp;
-        workers.size--;
-        if (workers.size == 0)
-        {
-            workers.empty = true;
-        }
-    }
     count_visited = 0;
     mtx_unlock(&lock);
     mtx_destroy(&lock);
@@ -98,205 +83,46 @@ void destroyQueue(void)
 
 void enqueue(void *t)
 {
-    Node *tmp;
-    size_t i;
+
     mtx_lock(&lock);
 
-    // We do not need to check malloc.
-    tmp = malloc(sizeof(Node));
-    tmp->data = t;
-    tmp->next = NULL;
+    enterJobToJobsQueue(t);
 
-    if (jobs.empty)
-    {
-        jobs.first = tmp;
-        jobs.last = jobs.first;
-        jobs.empty = false;
-    }
-    else
-    {
-        jobs.last->next = tmp;
-        jobs.last = tmp;
-    }
-    jobs.size++;
-
-    // Need to send signal to wake up the first worker in the workers queue.
+    // Need to send signal to wake up worker in the workers queue.
     if (!workers.empty && workers.size >= jobs.size)
     {
-        // Need to do a walk to the specific worker.
-        tmp = workers.first;
-        for (i = 1; i < jobs.size; ++i)
+        if (wakeUpWorker() != 0)
         {
-            tmp = tmp->next;
+            printf("an error have been occurs sending a signal.\n");
         }
-        cnd_signal((cnd_t *)tmp->data);
     }
-    // printf("(jobs : %zu, workers :  %zu) \n", jobs.size, workers.size);
+
     mtx_unlock(&lock);
 }
 
-// void enqueue(void *t)
-// {
-//     Node *tmp;
-//     mtx_lock(&lock);
-
-//     // We do not need to check malloc.
-//     tmp = malloc(sizeof(Node));
-//     tmp->data = t;
-//     tmp->next = NULL;
-
-//     if (jobs.empty)
-//     {
-//         jobs.first = tmp;
-//         jobs.last = jobs.first;
-//         jobs.empty = false;
-//     }
-//     else
-//     {
-//         jobs.last->next = tmp;
-//         jobs.last = tmp;
-//     }
-//     jobs.size++;
-
-//     // Need to send signal to wake up the first worker in the workers queue.
-//     if (!workers.empty )
-//     {
-//         // need to do a walk to the spesific worker.
-//         // now the thread on the dequeue can done before enqueue ending safe ?
-//         cnd_signal((cnd_t *)workers.first->data);
-//     }
-//     printf("(jobs : %zu, workers :  %zu) \n", jobs.size, workers.size);
-//     mtx_unlock(&lock);
-// }
-
 void *dequeue(void)
 {
-    size_t i;
-    cnd_t thread_hold;
-    Node *tmp;
-    Node *prev;
     void *t;
 
     mtx_lock(&lock);
-
-    //  ====================================================================================
-    // The worker can do his job.
     if (workers.size < jobs.size)
     {
-        tmp = jobs.first;
-        prev = NULL;
-        for (i = 1; i <= workers.size; ++i)
-        {
-            prev = tmp;
-            tmp = tmp->next;
-        }
-
-        t = tmp->data;
-        if (jobs.size == 1)
-        {
-            jobs.first = NULL;
-            jobs.last = NULL;
-        }
-        else
-        {
-            if (tmp->next == NULL)
-            {
-                jobs.last = prev;
-                jobs.last->next = NULL;
-            }
-            else
-            //
-            {
-                if (prev == NULL)
-                {
-                    jobs.first = tmp->next;
-                }
-                else
-                {
-                    prev->next = tmp->next;
-                }
-            }
-        }
-
-        jobs.size--;
-        if (jobs.size == 0)
-        {
-            jobs.empty = true;
-        }
-
-        free(tmp);
-        mtx_unlock(&lock);
-        return t;
+        t = getAJob();
     }
 
     else
     {
-
-        tmp = malloc(sizeof(Node));
-        tmp->data = &thread_hold;
-        tmp->next = NULL;
-        if (!workers.empty || jobs.empty)
-        {
-            // jobs.empty
-            if (workers.empty)
-            {
-                workers.first = tmp;
-                workers.last = tmp;
-                workers.empty = false;
-            }
-            else
-            {
-                workers.last->next = tmp;
-                workers.last = tmp;
-            }
-
-            workers.size++;
-            cnd_init(&thread_hold);
-            cnd_wait(&thread_hold, &lock);
-
-            tmp = workers.first;
-            workers.first = workers.first->next;
-            workers.size--;
-            if (workers.size == 0)
-            {
-                workers.empty = true;
-            }
-            free(tmp);
-        }
-
-        /*
-        If the 'if boolean' is false it is mean: "workers.empty && !jobs.empty  == true"
-        otherwise we enter the if and now we deal with one job at least in jobs queue,
-        because in enqueue we send a signal to the thread/worker ask for
-        a job, and we notify him that jobs has arrive.
-        We have one worker at least that can do the job(the one that call to the function).
-        With confident we can return the first job in the jobs queue.
-        */
-
-        tmp = jobs.first;
-        jobs.first = jobs.first->next;
-        jobs.size--;
-        if (jobs.size == 0)
-        {
-            jobs.empty = true;
-        }
-
-        t = tmp->data;
+        t = waitForAJob();
     }
 
-    free(tmp);
-    //  =====================================================================================
-
-    mtx_unlock(&lock);
     count_visited++;
+    mtx_unlock(&lock);
+
     return t;
 }
 
 bool tryDequeue(void **t)
 {
-    size_t i;
-    Node *tmp;
-    Node *prev;
 
     mtx_lock(&lock);
 
@@ -308,53 +134,15 @@ bool tryDequeue(void **t)
 
     if (workers.size < jobs.size)
     {
-        tmp = jobs.first;
-        prev = NULL;
-        for (i = 1; i <= workers.size; ++i)
-        {
-            prev = tmp;
-            tmp = tmp->next;
-        }
+        // Need to check
+        *t = getAJob();
 
-        *t = tmp->data;
-        if (jobs.size == 1)
-        {
-            jobs.first = NULL;
-            jobs.last = NULL;
-        }
-        else
-        {
-            if (tmp->next == NULL)
-            {
-                jobs.last = prev;
-                jobs.last->next = NULL;
-            }
-            else
-            {
-
-                if (prev == NULL)
-                {
-                    jobs.first = tmp->next;
-                }
-                else
-                {
-                    prev->next = tmp->next;
-                }
-            }
-        }
-
-        jobs.size--;
-        if (jobs.size == 0)
-        {
-            jobs.empty = true;
-        }
-
-        free(tmp);
         mtx_unlock(&lock);
         count_visited++;
         return true;
     }
 
+    mtx_unlock(&lock);
     return false;
 }
 
@@ -370,4 +158,279 @@ size_t waiting(void)
 size_t visited(void)
 {
     return count_visited;
+}
+
+void initJobs(void)
+{
+    jobs.first = NULL;
+    jobs.last = NULL;
+    jobs.size = 0;
+    jobs.empty = true;
+}
+
+void initWorkers(void)
+{
+    workers.first = NULL;
+    workers.last = NULL;
+    workers.size = 0;
+    workers.empty = true;
+}
+
+void destroyJobs(void)
+{
+
+    Node *tmp;
+    while (!jobs.empty)
+    {
+        tmp = jobs.first->next;
+        free(jobs.first);
+        jobs.first = tmp;
+        jobs.size--;
+        if (jobs.size == 0)
+        {
+            jobs.empty = true;
+        }
+    }
+}
+
+void destroyWorkers(void)
+{
+
+    Node *tmp;
+    while (!workers.empty)
+    {
+        tmp = workers.first->next;
+        free(workers.first);
+        workers.first = tmp;
+        workers.size--;
+        if (workers.size == 0)
+        {
+            workers.empty = true;
+        }
+    }
+}
+
+/*
+There is more jobs then workers,
+To workers have demand (and we are in happy times).
+*/
+void *getAJob(void)
+{
+    size_t i;
+    Node *tmp;
+    void *t;
+
+    tmp = jobs.first;
+    for (i = 1; i <= workers.size; ++i)
+    {
+        tmp = tmp->next;
+    }
+
+    t = tmp->data;
+    removeJob(tmp);
+    free(tmp);
+    return t;
+}
+
+/*
+There is more workers then jobs,
+We are living at awful times.
+*/
+void *waitForAJob(void)
+{
+
+    cnd_t thread_hold;
+    cnd_t *job_thread_hold;
+    Node *tmp;
+    void *t;
+
+    tmp = malloc(sizeof(Node));
+    tmp->next = NULL;
+    tmp->data = NULL;
+
+    enterWorker(tmp);
+
+    cnd_init(&thread_hold);
+    tmp->thread_hold = &thread_hold;
+    cnd_wait(&thread_hold, &lock);
+
+    // It is time to get a job.
+    // First remove the worker from the queue.
+
+    removeWorker(tmp);
+
+    job_thread_hold = tmp->thread_hold;
+
+    free(tmp);
+
+    tmp = jobs.first;
+
+    while (tmp->thread_hold != job_thread_hold)
+    {
+        tmp = tmp->next;
+    }
+    // Then remove the job from the queue.
+    removeJob(tmp);
+
+    t = tmp->data;
+
+    free(tmp);
+    return t;
+}
+
+/*
+There is more workers then jobs, so the last jobs enter can be done.
+*/
+int wakeUpWorker(void)
+{
+    Node *tmp;
+    size_t i;
+    int rc;
+
+    tmp = workers.first;
+    for (i = 1; i < jobs.size; ++i)
+    {
+        tmp = tmp->next;
+    }
+
+    rc = cnd_signal((cnd_t *)tmp->thread_hold);
+    if (thrd_success == rc)
+    {
+        // probelm with the
+        jobs.last->thread_hold = tmp->thread_hold;
+        return 0;
+    }
+
+    return 1;
+}
+
+void enterJobToJobsQueue(void *t)
+{
+    Node *tmp;
+    tmp = malloc(sizeof(Node));
+    tmp->data = t;
+    tmp->next = NULL;
+    tmp->thread_hold = NULL;
+
+    if (jobs.empty)
+    {
+        jobs.first = tmp;
+        jobs.last = jobs.first;
+        jobs.empty = false;
+    }
+    else
+    {
+        jobs.last->next = tmp;
+        jobs.last = tmp;
+    }
+    jobs.size++;
+}
+
+void removeJob(void *node)
+{
+    size_t i;
+    Node *tmp;
+    Node *prev = NULL;
+
+    tmp = jobs.first;
+    for (i = 1; i < jobs.size; ++i)
+    {
+        if (tmp == node)
+        {
+            break;
+        }
+
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    if (prev == NULL)
+    {
+        if (jobs.size == 1)
+        {
+            jobs.first = NULL;
+            jobs.first = NULL;
+        }
+        else
+        {
+            jobs.first = jobs.first->next;
+        }
+    }
+    else
+    {
+        if (tmp == jobs.last)
+        {
+            jobs.last = prev;
+        }
+        prev->next = tmp->next;
+    }
+
+    jobs.size--;
+    if (jobs.size == 0)
+    {
+        jobs.empty = true;
+    }
+}
+
+void removeWorker(void *node)
+{
+    size_t i;
+    Node *tmp;
+    Node *prev = NULL;
+
+    tmp = workers.first;
+    for (i = 1; i < workers.size; ++i)
+    {
+
+        if (tmp == node)
+        {
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    if (prev == NULL)
+    {
+        if (workers.size == 1)
+        {
+            workers.first = NULL;
+            workers.first = NULL;
+        }
+        else
+        {
+            workers.first = workers.first->next;
+        }
+    }
+    else
+    {
+        if (tmp == workers.last)
+        {
+            workers.last = prev;
+        }
+        prev->next = tmp->next;
+    }
+
+    workers.size--;
+    if (workers.size == 0)
+    {
+        workers.empty = true;
+    }
+}
+
+void enterWorker(void *node)
+{
+    if (workers.empty)
+    {
+        workers.first = node;
+        workers.last = node;
+        workers.empty = false;
+    }
+
+    else
+    {
+        workers.last->next = node;
+        workers.last = node;
+    }
+    workers.size++;
 }
